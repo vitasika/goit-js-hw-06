3e62e295b3ff4256efe46aab9e99fa47789905db
vitasika <vitasika1@gmail.com>
2022-01-09 22:52:27 +0200

    делаю категории с их количеством

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,3 +1,17 @@
 const listWithClass = document.querySelectorAll('.item');
 console.log('Number of categories: ', listWithClass.length);
 
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+listWithClass.forEach(  el => { 
+
+    // console.log(el)
+    // el.getAttribute('h2');
+    // console.log(el.getAttribute('h2'))
+    console.log(el.childNodes)
+
+    el.childNodes.find()
+
+})
