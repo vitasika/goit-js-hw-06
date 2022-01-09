d15601d79afdc4e07bb84f01bd6db8662a2a4d4d
vitasika <vitasika1@gmail.com>
2022-01-10 00:40:18 +0200

    закончил 1 задание

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,15 +1,18 @@
 // Показывает число категорий
 const listWithClass = document.querySelectorAll('.item');
+
 console.log('Number of categories: ', listWithClass.length);
 
 // Показывает название и часло элементов в категории
-listWithClass.forEach(  el => { 
+listWithClass.forEach(el => {
 
     // Показывает название категории
-    console.log('Categories: ', el.childNodes[1].textContent)
+    // console.log('Categories:',el.childNodes[1].textContent)
 
-    // Показывает количество элементов в категории
-    console.log('Elements: ', el.childNodes[3]);
+    console.log('Categories: ', el.getElementsByTagName('h2').item(0).innerText);
     
+    // Показывает количество элементов в категории    
+    console.log('Elements: ',el.getElementsByTagName('li').length);
 
 })
+
