fa820133a1d5e1a9b3003bf6043db7314d9c6a12
vitasika <vitasika1@gmail.com>
2022-01-16 22:49:44 +0200

    нашел строчки в html задание 5

--- js/lesson-5.js
+++ js/lesson-5.js
@@ -4,3 +4,14 @@
 
 <input type="text" id="name-input" placeholder="Please enter your name" />
 <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */
+
+
+//НАхожу строчку input в документе html для ввода текста
+const inputEl = document.querySelector('#name-input');
+console.log(inputEl);
+
+//НАхожу строчку span в документе html для ввода текста каторый набирается в input
+const spanEl = document.querySelector('#name-output');
+console.log(spanEl);
+
+
