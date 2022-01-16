6466f1933878ca924a987d3b4500d041f3a3f39e
vitasika <vitasika1@gmail.com>
2022-01-16 23:35:20 +0200

    закончил 5 задание

--- js/lesson-5.js
+++ js/lesson-5.js
@@ -10,8 +10,24 @@
 const inputEl = document.querySelector('#name-input');
 console.log(inputEl);
 
-//НАхожу строчку span в документе html для ввода текста каторый набирается в input
+// Нахожу строчку span в документе html для ввода текста каторый набирается в input
 const spanEl = document.querySelector('#name-output');
 console.log(spanEl);
 
+// Нужно повесить слушателя события на input (вводимый текст в строку input) 
+// и вызвать коллбек функцию которая будет записывать в span вводимое значение.
+inputEl.addEventListener('input', inputSpanEl);
+
+
+function inputSpanEl(event) {
+    if (event.currentTarget.value === '') {
+
+        // Возвращает в span текс Anonymous
+        return spanEl.textContent = 'Anonymous';
+    };
+
+    // Записывает в строку вводимое значение из input строки при вводе
+    return spanEl.textContent = event.currentTarget.value;
+    
+}
 
