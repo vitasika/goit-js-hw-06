3283ba00a6dddb4a81a9a60136da817313b068a4
vitasika <vitasika1@gmail.com>
2022-01-20 22:00:26 +0200

    сделал 8 домашку

--- js/lesson-8.js
+++ js/lesson-8.js
@@ -15,14 +15,43 @@
 </form>
 Обработка отправки формы form.login-form должна быть по событию submit.
 При отправке формы страница не должна перезагружаться.
-Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть
+заполнены.
+Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля 
+будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы 
+используй свойство elements.
 Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 */
 
+// Нахожу строку в html
+const formEl = document.querySelector(`.login-form`);
+
+// Вешаем слушателя на formEl (submit)
+formEl.addEventListener('submit', click);
 
 
+function click(event) {
 
+  event.preventDefault();
+
+  const inputElemail = document.querySelector('#email');
+  //console.log(inputElemail.value);
+
+  const inputElpass = document.querySelector('#password');
+  //console.log(inputElpass.value);
+
+  const retValue = {'email': inputElemail.value, 'password': inputElpass.value}
+  //console.log(retValue);
+
+  if (retValue.email === '' || retValue.password === '') {
+    return alert('Все поля должны быть заполнены!!!');
+  }
+
+  else { console.log(retValue); }
+
+  // очищаем поле формы методом reset
+  event.currentTarget.reset();
+}; 
 
 
 
